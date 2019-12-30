b53b888ee37b8874a2a7d408a8cb835f1efb0a42
DEVfancybear <duonghacker2345@gmail.com>
2019-12-30 11:21:21 +0700

    heroku app 4

--- index.js
+++ index.js
@@ -19,11 +19,10 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 if (process.env.NODE_ENV === "production") {
-  // Serve any static files
   app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  //
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
   });
 }
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
