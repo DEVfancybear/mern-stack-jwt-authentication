eef769751075ec16dad77cafe5cb4b242f7c13c3
DEVfancybear <duonghacker2345@gmail.com>
2019-12-30 10:57:20 +0700

    heroku app 1

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -14,14 +14,14 @@ const Landing = ({ isAuthenticated }) => {
             Create a developer profile/portfolio, share posts and get help from
             other developers
           </p>
-          <div className="buttons">
+          {/* <div className="buttons">
             <Link to="/register" className="btn btn-primary">
               Sign Up
             </Link>
             <Link to="/login" className="btn btn-light">
               Login
             </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
 const port = process.env.PORT || 5000;
 
 // connect database
@@ -18,4 +19,10 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
